35819fd0ae48f241694585e6600b11476ae58dae
Payam Naderi <naderi.payam@gmail.com>
2017-05-01 15:21:51 +0430

    expected result json

---
+++ Response/ExpectedJson.php
@@ -0,0 +1,28 @@
+<?php
+namespace Poirot\ApiClient\Response;
+
+use Poirot\ApiClient\Interfaces\Response\iResponse;
+use Poirot\Std\Struct\DataEntity;
+
+
+class ExpectedJson
+    extends aExpectedResponse
+{
+    /**
+     * Get Expected Result From Response Raw Body
+     *
+     * @param string    $originResult Raw Response Body
+     * @param iResponse $self
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    function __invoke($originResult, iResponse $self)
+    {
+        if (false === $result = json_decode($originResult, true))
+            throw new \Exception('Server Response Cant Parse To Json.');
+
+
+        return new DataEntity($result);
+    }
+}
