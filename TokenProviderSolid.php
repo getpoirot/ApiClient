f652c2dc043c8020928db51e8e3e1d24e518c7c6
Payam Naderi <naderi.payam@gmail.com>
2017-05-07 13:31:27 +0430

    implement solid token provider

---
+++ TokenProviderSolid.php
@@ -0,0 +1,46 @@
+<?php
+namespace Poirot\ApiClient;
+
+use Poirot\ApiClient\Interfaces\Token\iAccessTokenObject;
+use Poirot\ApiClient\Interfaces\Token\iTokenProvider;
+
+
+class TokenProviderSolid
+    implements iTokenProvider
+{
+    /** @var iAccessTokenObject */
+    protected $token;
+
+
+    /**
+     * TokenProviderSolid constructor.
+     *
+     * @param iAccessTokenObject $token
+     */
+    function __construct(iAccessTokenObject $token)
+    {
+        $this->token = $token;
+    }
+
+
+    /**
+     * Get Current Token if Not Exchange New one
+     *
+     * @return iAccessTokenObject
+     */
+    function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Exchange New Token
+     *
+     * @return iAccessTokenObject
+     * @throws \Exception
+     */
+    function exchangeToken()
+    {
+        throw new \Exception('Solid Token Exchange Not Implemented.');
+    }
+}
